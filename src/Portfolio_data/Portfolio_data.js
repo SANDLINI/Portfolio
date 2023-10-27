0dc2c49678a8b28f35a67d76807270d07f68658a
SANDLINI <dominictorreto870@gmial.com>
2023-10-27 19:41:17 +0530

    Added a new project Zaika

--- src/Portfolio_data/Portfolio_data.js
+++ src/Portfolio_data/Portfolio_data.js
@@ -1,20 +1,20 @@
 import carImage from '../assets/car.png';
 import eshopImage from '../assets/eshop.png';
 import movieImage from '../assets/movie.png';
-import trippyImage from '../assets/trippy.png';
+import zaikaImage from '../assets/zaika.png';
 
 
 export const Portfolio_data = [
     {
         heading: 'Car Rental',
         image: carImage,
-        description: 'A car rental front end project made in React.js and material UI, which is fully responsive. I have used all the advanced concepts mainly Routing and client side validations and added many other functionality to make the project user friendly.',
+        description: 'A car rental website build in React.js. User can book their ride as well as choose their ride based on their likings and comfort from the wide range of ride options. A form is also provided for the booking details and if they have any queries for that contact is also provided. The website is completely responsive and mobile friendly.',
         link: 'https://sandlini-car-rental.netlify.app'
     },
     {
         heading: 'E-Shop',
         image: eshopImage,
-        description: 'A fully functional front end E-commerce website built in React.js and for styling Material UI ( MUI ) is used and for state management redux-toolkit is used. The website is fully responsive and the data persists even after the session ends. All the advanced features are included in this project.',
+        description: 'A fully functional front end E-commerce website built in React.js. User can check products based on their linkings and can see the detail of the product. User can add the products to the cart where they can increase, decrease and remove that item from the cart. The website is fully responsive and the data persists even after the session ends.',
         link: 'https://sandlini-e-shop.netlify.app',
 
     },
@@ -22,16 +22,16 @@ export const Portfolio_data = [
     {
         heading: 'Movie',
         image: movieImage,
-        description: 'A beginner React.js project. TMDB movie API is used to fetch data. Project is built in React.js and for styling I have used material UI. Project is fully responsive and mobile friendly.',
+        description: 'A movie search application made in React.js for movie lovers. User can chekc the popular upcoming and latest movies. They can go to the movie detail page to get movie details. Application is fully responsive and mobile friendly.',
         link: 'https://sandlini-movie.netlify.app',
 
     },
 
     {
-        heading: "Trippy",
-        image: trippyImage,
-        description: 'A beginner project made in React js with material UI. The project is fully responsive. Many components have been reused and data is passed down through props. All major concepts of React has been used mainly focused on reusable components.',
-        link: 'https://sandlini-trippy.netlify.app',
+        heading: "Zaika",
+        image: zaikaImage,
+        description: 'A recipe search application built in React.js. User can search for their favorite recipe and see the cooking instructions. They can add recipes to the favorite for the later use. ',
+        link: 'https://sandlini-zaika.netlify.app',
     }
 ]
 
